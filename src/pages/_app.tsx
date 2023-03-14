85b867d7c2eff862ae4e18ee9621103ab738aea7
Bruno <bruno_moises_stz@hotmail.com>
2023-03-14 17:03:59 -0300

    reorginize folders of project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import '@/styles/globals.css'
-import { darkTheme } from '@/styles/themes/dark'
+import '@/src/styles/globals.css'
+import { darkTheme } from '@/src/styles/themes/dark'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+import AppTemplate from '../components/AppTemplate'
 import GlobalStyles from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <AppTemplate>
+        <Component {...pageProps} />
+      </AppTemplate>
     </ThemeProvider>
   )
 }
