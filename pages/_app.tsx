448f18a62f006837f12ddf1753ad252d84d5bd8e
Bruno <bruno_moises_stz@hotmail.com>
2023-03-14 14:41:25 -0300

    apply theme in body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
+import { darkTheme } from '@/styles/themes/dark'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
