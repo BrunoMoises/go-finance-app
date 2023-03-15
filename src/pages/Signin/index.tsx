5bd1056a2c97b3990611945dec3e0ce7a0d132bd
Bruno <bruno_moises_stz@hotmail.com>
2023-03-15 17:34:49 -0300

    create login page

---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import { Container, Content } from "./styles"
+import logo from '../../assets/logo.png'
+import Link from "next/link"
+
+const Signin = () => (
+  <Container>
+    <Content>
+      <Image src={logo} alt="GoFinance" width={150} />
+      <input type="text" placeholder="Nome de usuário" autoComplete="off" />
+      <input type="password" placeholder="Senha do usuário" autoComplete="off" />
+      <button>Entrar</button>
+      <p>Ainda não possui uma conta? <Link href="/Signup">Cadastre-se</Link></p>
+    </Content>
+  </Container>
+)
+
+export default Signin
