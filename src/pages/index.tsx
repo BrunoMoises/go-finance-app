3be25e24662c5d75ced2582d68f0f48b93db087d
Bruno <bruno_moises_stz@hotmail.com>
2023-03-15 15:47:21 -0300

    finalization of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,32 @@
+import { Card } from "../components/Card";
 import { Chart } from "../components/Chart";
+import { Container, RightSide } from "./styles";
 
-const Home = () =>
-  <Chart />;
+const cardData = [
+  {
+    title: "Saldo a pagar",
+    value: "120,75"
+  },
+  {
+    title: "Saldo a receber",
+    value: "120,75"
+  },
+  {
+    title: "Saldo total",
+    value: "120,75"
+  },
+];
+
+const Home = () => (
+  <Container>
+    <Chart />
+    <RightSide>
+      {cardData.map(card => (
+        <Card key={card.title} title={card.title} value={card.value} />
+      ))}
+      <button>Download</button>
+    </RightSide>
+  </Container>
+);
 
 export default Home;
