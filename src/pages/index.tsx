cc9ba64d3e547d73d1a6fcf481e1a15d45537fdf
Bruno <bruno_moises_stz@hotmail.com>
2023-03-15 11:39:14 -0300

    create standard of buttons and inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { Card } from "../components/Card";
 const Home = () => (
   <>
     <Card title="Saldo a pagar" value="R$ 10.000,00" />
+    <button>teste</button>
+    <input type="text" name="" id="" placeholder="saldo a pagar..." />
   </>
 );
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,4 +11,35 @@ export default createGlobalStyle`
         background-color: ${(props) => props.theme.colors.black2};
         color: ${(props) => props.theme.colors.white};
     }
+
+    button {
+        background-color: ${(props) => props.theme.colors.primary};
+        padding: 16px;
+        border-radius: 8px;
+        font-size: 25px;
+        font-weight: bold;
+        color: ${(props) => props.theme.colors.white};
+        cursor: pointer;
+        transition: all 1;
+
+        &:hover {
+            background-color: ${(props) => props.theme.colors.secondary};
+        }
+    }
+
+    input {
+        background-color: ${(props) => props.theme.colors.black3};
+        color: ${(props) => props.theme.colors.white};
+        border: none;
+        min-height: 27px;
+        border-radius: 4px;
+        padding-left: 16px;
+        padding-top: 8px;
+        padding-bottom: 8px;
+        ::-webkit-input-placeholder {
+            color: #4b4747;
+            font-weight: 500;
+            font-size: 12px;
+        }
+    }
 `;
