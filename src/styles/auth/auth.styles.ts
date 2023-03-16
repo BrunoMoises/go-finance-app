19fc3e1f8d3ead29531d15317650184f451e15a5
Bruno <bruno_moises_stz@hotmail.com>
2023-03-16 15:38:31 -0300

    refactor structure for styles of auth in unique archive

--- src/styles/auth/auth.styles.ts
+++ src/styles/auth/auth.styles.ts
@@ -12,7 +12,7 @@ export const Content = styled.div`
     border-radius: 16px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     width: 45%;
-    height: 65%;
+    height: 75%;
     display: flex;
     justify-content: center;
     flex-direction: column;
