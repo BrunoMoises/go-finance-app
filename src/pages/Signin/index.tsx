1f4d0b764cf055f18127f0f83ddf2063611ede85
Bruno <bruno_moises_stz@hotmail.com>
2023-03-17 16:00:29 -0300

    add function of login

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -2,17 +2,25 @@ import Image from "next/image"
 import { Container, Content } from "../../styles/auth/auth.styles"
 import logo from '../../assets/logo.png'
 import Link from "next/link"
+import { useState } from "react"
+import useLogin from "@/src/Hooks/useLogin"
 
-const Signin = () => (
-  <Container>
-    <Content>
-      <Image src={logo} alt="GoFinance" width={150} />
-      <input type="text" placeholder="Nome de usuário" autoComplete="off" />
-      <input type="password" placeholder="Senha do usuário" autoComplete="off" />
-      <button>Entrar</button>
-      <p>Ainda não possui uma conta? <Link href="/Signup">Cadastre-se</Link></p>
-    </Content>
-  </Container>
-)
+const Signin = () => {
+  const { login } = useLogin();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = () => login({ username, password });
+  return (
+    <Container>
+      <Content>
+        <Image src={logo} alt="GoFinance" width={150} />
+        <input type="text" placeholder="Nome de usuário" autoComplete="off" onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Senha do usuário" autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={signIn}>Entrar</button>
+        <p>Ainda não possui uma conta? <Link href="/Signup">Cadastre-se</Link></p>
+      </Content>
+    </Container>
+  )
+}
 
 export default Signin
