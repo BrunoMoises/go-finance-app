0fa2f019d9ff2809b2003eb7fe989b75244945ac
Bruno <bruno_moises_stz@hotmail.com>
2023-03-23 14:45:09 -0300

    feat: add error tratment of signin

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { Container, Content } from "../../styles/auth/auth.styles"
+import { Container, Content, ErrorMessage, FormContainer } from "../../styles/auth/auth.styles"
 import logo from '../../assets/logo.png'
 import Link from "next/link"
 import { useState } from "react"
@@ -9,15 +9,27 @@ const Signin = () => {
   const { login } = useLogin();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const signIn = () => login({ username, password });
+  const [errorMessage, setErrorMessage] = useState("");
+  const signIn = () => {
+    try {
+      login({ username, password })
+    } catch (error: any) {
+      const userNotFound = error.response?.status === 404;
+      if (userNotFound)
+        setErrorMessage("Usuário e/ou senha não encontrado.");
+    }
+  }
   return (
     <Container>
       <Content>
         <Image src={logo} alt="GoFinance" width={150} />
-        <input type="text" placeholder="Nome de usuário" autoComplete="off" onChange={(e) => setUsername(e.target.value)} />
-        <input type="password" placeholder="Senha do usuário" autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
-        <button onClick={signIn}>Entrar</button>
-        <p>Ainda não possui uma conta? <Link href="/Signup">Cadastre-se</Link></p>
+        <FormContainer>
+          <input type="text" placeholder="Nome de usuário" autoComplete="off" onChange={(e) => setUsername(e.target.value)} />
+          <input type="password" placeholder="Senha do usuário" autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
+          <button onClick={signIn}>Entrar</button>
+          <ErrorMessage>{errorMessage}</ErrorMessage>
+          <p>Ainda não possui uma conta? <Link href="/Signup">Cadastre-se</Link></p>
+        </FormContainer>
       </Content>
     </Container>
   )
