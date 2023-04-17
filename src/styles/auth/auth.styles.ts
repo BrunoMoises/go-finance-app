ea364f16366dedab3a8302976efdc28bf2117a19
Bruno <bruno_moises_stz@hotmail.com>
2023-04-17 20:12:21 -0300

    style: remove desnecessary parameter

--- src/styles/auth/auth.styles.ts
+++ src/styles/auth/auth.styles.ts
@@ -10,7 +10,7 @@ export const Container = styled.div`
 
 export const Content = styled.div`
     border-radius: 16px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
     width: 45%;
     display: flex;
     justify-content: center;
