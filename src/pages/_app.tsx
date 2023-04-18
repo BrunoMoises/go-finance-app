27e40c8702b73cce62538300525ef0c8a495ca61
Bruno <bruno_moises_stz@hotmail.com>
2023-04-18 08:40:35 -0300

    feat: install and implement react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from 'styled-components'
 import AppTemplate from '../components/AppTemplate'
 import AuthProvider from '../providers/auth'
 import GlobalStyles from '../styles/global'
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -20,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <AppTemplate>
           <Component {...pageProps} />
         </AppTemplate>
+        <ToastContainer theme="dark"/>
       </ThemeProvider>
     </AuthProvider>
   )
