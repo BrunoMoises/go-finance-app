b31a2a6e12d1b825eb9fbfd475cb397e4ee87de0
Bruno <bruno_moises_stz@hotmail.com>
2023-04-17 22:21:43 -0300

    feat: add modal to create new category

--- src/components/TableNavBar/index.tsx
+++ src/components/TableNavBar/index.tsx
@@ -1,14 +1,23 @@
-import { DateInput } from "../DateInput";
-import { SearchBar } from "../SearchBar";
-import { Container, LeftSide } from "./styles";
+import {DateInput} from "../DateInput";
+import {SearchBar} from "../SearchBar";
+import {Container, LeftSide} from "./styles";
+import Modal from "@/src/components/Modal";
 
-export const TableNavBar = () => (
+export interface ModalProps {
+    title: string;
+    buttonTitle: string;
+    setTitle: any;
+    setDescription: any;
+    createCategoryHandle: any;
+}
+
+export const TableNavBar = ({title, buttonTitle, setTitle, setDescription, createCategoryHandle}: ModalProps) => (
     <Container>
         <LeftSide>
-            <DateInput title="Data Inicial" />
-            <DateInput title="Data Final" />
-            <SearchBar />
+            <DateInput title="Data Inicial"/>
+            <DateInput title="Data Final"/>
+            <SearchBar/>
         </LeftSide>
-        <button>Adicionar</button>
+        <Modal title={title} buttonTitle={buttonTitle} setDescription={setDescription} setTitle={setTitle} createCategoryHandle={createCategoryHandle}/>
     </Container>
 )
