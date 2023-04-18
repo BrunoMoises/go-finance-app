74f787e160c24fb2b36c503d4d62fc6987fb7674
Bruno <bruno_moises_stz@hotmail.com>
2023-04-18 08:43:06 -0300

    refactor: specify type from props

--- src/components/TableNavBar/index.tsx
+++ src/components/TableNavBar/index.tsx
@@ -2,13 +2,14 @@ import {DateInput} from "../DateInput";
 import {SearchBar} from "../SearchBar";
 import {Container, LeftSide} from "./styles";
 import Modal from "@/src/components/Modal";
+import {Dispatch, SetStateAction} from "react";
 
 export interface ModalProps {
     title: string;
     buttonTitle: string;
-    setTitle: any;
-    setDescription: any;
-    createCategoryHandle: any;
+    setTitle: Dispatch<SetStateAction<string>>;
+    setDescription: Dispatch<SetStateAction<string>>;
+    createCategoryHandle: () => Promise<void>;
 }
 
 export const TableNavBar = ({title, buttonTitle, setTitle, setDescription, createCategoryHandle}: ModalProps) => (
@@ -18,6 +19,7 @@ export const TableNavBar = ({title, buttonTitle, setTitle, setDescription, creat
             <DateInput title="Data Final"/>
             <SearchBar/>
         </LeftSide>
-        <Modal title={title} buttonTitle={buttonTitle} setDescription={setDescription} setTitle={setTitle} createCategoryHandle={createCategoryHandle}/>
+        <Modal title={title} buttonTitle={buttonTitle} setDescription={setDescription} setTitle={setTitle}
+               createCategoryHandle={createCategoryHandle}/>
     </Container>
 )
