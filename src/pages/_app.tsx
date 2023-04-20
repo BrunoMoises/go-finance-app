06a382b3ce83d2e2ff783044095e3b34e5cb5872
Bruno <bruno_moises_stz@hotmail.com>
2023-04-20 09:04:45 -0300

    fix: correction head location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,11 +13,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <ThemeProvider theme={darkTheme}>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
-        </Head>
         <GlobalStyles />
         <AppTemplate>
           <Component {...pageProps} />
